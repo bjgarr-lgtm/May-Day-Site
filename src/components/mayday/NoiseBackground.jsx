180b75ed26914a19218067a73e406718b6aed4e1
bjgarr-lgtm <bjgarr@gmail.com>
2026-03-29 21:59:56 -0700

    Update NoiseBackground.jsx

--- src/components/mayday/NoiseBackground.jsx
+++ src/components/mayday/NoiseBackground.jsx
@@ -4,7 +4,8 @@ export default function NoiseBackground() {
   return (
     <>
       <div className="pointer-events-none fixed inset-0 opacity-[0.11] mix-blend-screen [background-image:radial-gradient(circle_at_20%_20%,rgba(255,255,255,.65)_0,transparent_18%),radial-gradient(circle_at_80%_10%,rgba(255,255,255,.25)_0,transparent_16%),radial-gradient(circle_at_40%_70%,rgba(255,255,255,.35)_0,transparent_14%),radial-gradient(circle_at_75%_80%,rgba(255,255,255,.2)_0,transparent_16%)]" />
-      <div className="pointer-events-none fixed inset-0 opacity-[0.16] mix-blend-overlay [background-image:url('data:image/svg+xml;utf8,<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"160\" height=\"160\" viewBox=\"0 0 160 160\"><filter id=\"n\"><feTurbulence type=\"fractalNoise\" baseFrequency=\"1.1\" numOctaves=\"2\" stitchTiles=\"stitch\"/></filter><rect width=\"160\" height=\"160\" filter=\"url(%23n)\" opacity=\"0.75\"/></svg>')]" />
+      <div className="pointer-events-none fixed inset-0 opacity-[0.14] mix-blend-overlay [background-image:repeating-linear-gradient(0deg,rgba(255,255,255,.045)_0_1px,transparent_1px_3px),repeating-linear-gradient(90deg,rgba(0,0,0,.04)_0_1px,transparent_1px_4px)]" />
+      <div className="pointer-events-none fixed inset-0 opacity-[0.10] mix-blend-soft-light [background-image:radial-gradient(rgba(255,255,255,.12)_0.7px,transparent_0.7px)] [background-size:9px_9px]" />
       <div className="pointer-events-none fixed inset-0 bg-[linear-gradient(180deg,rgba(0,0,0,.18),transparent_18%,transparent_82%,rgba(0,0,0,.22))]" />
     </>
   )
