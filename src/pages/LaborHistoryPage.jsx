2ca3a30081f8e7757df20be5d2a3c5a1dd574c87
bjgarr-lgtm <bjgarr@gmail.com>
2026-03-30 03:01:45 -0700

    cscs

--- src/pages/LaborHistoryPage.jsx
+++ src/pages/LaborHistoryPage.jsx
@@ -1,6 +1,12 @@
-import LaborArchiveRoot from "../components/LaborArchiveRoot";
-import data from "../archive/laborArchiveData";
+import LaborArchiveRoot from '../components/LaborArchiveRoot'
+import laborArchiveData from '../archive/laborArchiveData'
 
 export default function LaborHistoryPage() {
-  return <LaborArchiveRoot collections={data} />;
+  return (
+    <LaborArchiveRoot
+      title="The Labor History of the Harbor"
+      subtitle="Scanned newspapers, strike history, labor violence, and regional working class memory."
+      collections={laborArchiveData}
+    />
+  )
 }
