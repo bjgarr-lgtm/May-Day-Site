a84e3694cfd2d66bbb9406310b3c892ffb5c3cc6
bjgarr-lgtm <bjgarr@gmail.com>
2026-03-31 23:27:14 -0700

    Update StopQRCode.jsx

--- src/components/StopQRCode.jsx
+++ src/components/StopQRCode.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 
 export default function StopQRCode({ value, title = 'Stop QR code' }) {
-  const src = `https://chart.googleapis.com/chart?cht=qr&chs=220x220&chl=${encodeURIComponent(value)}`
-
+  const src = `https://api.qrserver.com/v1/create-qr-code/?size=220x220&data=${encodeURIComponent(value)}`
   return (
     <div className="rounded-[1.5rem] border border-[#e3a7a5]/18 bg-black/20 p-5 sm:p-6">
       <p className="text-[10px] uppercase tracking-[0.18em] text-[#e3a7a5]/76">{title}</p>
