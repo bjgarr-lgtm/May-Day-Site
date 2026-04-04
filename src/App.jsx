945ad7c7c317668d60606b42f708b268bd9544d6
bjgarr-lgtm <bjgarr@gmail.com>
2026-04-04 13:47:05 -0700

    cs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
+import ScrollToTop from './components/ScrollToTop'
 import MayDayWelcomeCenter from './pages/MayDayWelcomeCenter'
 import HuntHome from './pages/HuntHome'
 import HuntStopPage from './pages/HuntStopPage'
@@ -11,17 +12,20 @@ import ApplicationsDashboardPage from './pages/ApplicationsDashboardPage'
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<MayDayWelcomeCenter />} />
-      <Route path="/hunt" element={<HuntHome />} />
-      <Route path="/hunt/:category/:stopId" element={<HuntStopPage />} />
-      <Route path="/labor-history" element={<LaborHistoryPage />} />
-      <Route path="/vendor-application" element={<VendorApplicationPage />} />
-      <Route path="/performer-application" element={<PerformerApplicationPage />} />
-      <Route path="/poster-art-contest" element={<PosterArtContestPage />} />
-      <Route path="/admin" element={<ApplicationsDashboardPage />} />
-      <Route path="/applications" element={<Navigate to="/admin" replace />} />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<MayDayWelcomeCenter />} />
+        <Route path="/hunt" element={<HuntHome />} />
+        <Route path="/hunt/:category/:stopId" element={<HuntStopPage />} />
+        <Route path="/labor-history" element={<LaborHistoryPage />} />
+        <Route path="/vendor-application" element={<VendorApplicationPage />} />
+        <Route path="/performer-application" element={<PerformerApplicationPage />} />
+        <Route path="/poster-art-contest" element={<PosterArtContestPage />} />
+        <Route path="/admin" element={<ApplicationsDashboardPage />} />
+        <Route path="/applications" element={<Navigate to="/admin" replace />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </>
   )
 }
